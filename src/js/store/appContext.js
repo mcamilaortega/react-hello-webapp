01cfb236b97574a92e627f8f4a71b1c61a7db8d1
Maria Camila Ortega <148597026+mcamilaortega@users.noreply.github.com>
2024-02-17 16:01:55 +0000

    done

--- src/js/store/appContext.js
+++ src/js/store/appContext.js
@@ -31,6 +31,11 @@ const injectContext = PassedComponent => {
 			 * state.actions.loadSomeData(); <---- calling this function from the flux.js actions
 			 *
 			 **/
+				state.actions.fetchPeople();
+				state.actions.fetchPlanets();
+				state.actions.fetchShips();
+
+
 		}, []);
 
 		// The initial value for the context is not null anymore, but the current state of this component,
